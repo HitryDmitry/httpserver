3e5a5dd9d16c9ebc5c94d35928fe6be713ada1da
Dmitry Gantimurov <66248017+HitryDmitry@users.noreply.github.com>
2025-03-27 11:42:37 +0300

    Delete build directory

--- build/CMakeFiles/httpserver.dir/httpserver_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/httpserver.dir/httpserver_autogen/mocs_compilation.cpp.o: \
- /home/dmitry/study/httpserver/build/httpserver_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitry/study/httpserver/tests
-# Build directory: /home/dmitry/study/httpserver/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
