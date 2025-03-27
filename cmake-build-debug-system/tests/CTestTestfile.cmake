6a76ba046f99e33aeaf45f3a502a59c4e37c7973
Dmitry Gantimurov <66248017+HitryDmitry@users.noreply.github.com>
2025-03-27 11:42:04 +0300

    Build directories deleted

--- cmake-build-debug-system/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitry/study/httpserver/tests
-# Build directory: /home/dmitry/study/httpserver/cmake-build-debug-system/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
